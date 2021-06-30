4e49e9b3a5a35765e0a20b0bf494d202448340ee
MaximeGrosmaire <64633980+MaximeGrosmaire@users.noreply.github.com>
2021-06-30 22:04:20 +1000

    Initial commit

---
+++ Source/Anchorhold/Anchorhold.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Anchorhold.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Anchorhold, "Anchorhold" );
---
+++ Source/Anchorhold/AnchorholdGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AnchorholdGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANCHORHOLD_API AAnchorholdGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
